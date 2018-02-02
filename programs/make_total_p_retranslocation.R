50980f550c1141900dfa8d1b9278fe9ea6fe86ea
mingkaijiang <shanyujordan512@gmail.com>
2018-02-02 12:02:33 +1100

    corrected a variable name

--- programs/make_total_p_retranslocation.R
+++ programs/make_total_p_retranslocation.R
@@ -2,8 +2,10 @@ make_total_p_retranslocation <- function(sumDF) {
     ### Calculate total p retranslocated
     ### canopy p - litterfall p + biomass P increment
     
-    retranDF <- (sumDF[sumDF$terms == "Wood P flux", 2:9] + sumDF[sumDF$terms == "Canopy P flux", 2:9] - 
-                     sumDF[sumDF$terms == "Leaflitter P flux", 2:9])
+    df1 <- sumDF[sumDF$term == "Wood P flux", 2:9] + sumDF[sumDF$term == "Canopy P flux", 2:9]
+    df2 <- df1 - sumDF[sumDF$term == "Leaflitter P flux", 2:9]
+    
+    retranDF <- df2
     
     retranDF <- as.data.frame(retranDF)
     
