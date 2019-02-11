d850cba14fdeb8cf64c3ac5732f04c870b763344
mingkaijiang <shanyujordan512@gmail.com>
2019-02-11 14:05:37 +1100

    corrected an error

--- modules/p_concentration_variables/canopy_p_pool/make_leafp_conc_treatment_abs_effect_statistics.R
+++ modules/p_concentration_variables/canopy_p_pool/make_leafp_conc_treatment_abs_effect_statistics.R
@@ -38,7 +38,8 @@ make_leafp_conc_treatment_abs_effect_statistics <- function(inDF,
     colnames(inDF)[var.col] <- "Value"
     
     ## Get year list and ring list
-    tDF <- summaryBy(Value+Cov2+Cov~Trt+Ring+Datef,data=inDF,FUN=mean, keep.names=T)
+    tDF <- summaryBy(Value+Cov2+Cov~Trt+Ring+Datef,data=inDF,FUN=mean, keep.names=T, na.rm=T)
+    tDF <-  tDF[tDF$Datef!="2012-05-11",]    
 
     ### Analyse the variable model
     ## model 1: no interaction, year as factor, ring random factor, include pre-treatment effect
@@ -59,20 +60,20 @@ make_leafp_conc_treatment_abs_effect_statistics <- function(inDF,
     
     ### Analyse the variable model
     ## model 2: interaction, year as factor, ring random factor, with pre-treatment
-    int.m2 <- "interative_with_covariate"
-    modelt2 <- lmer(Value~Trt*Datef+Cov2 + (1|Ring),data=tDF)
-    
-    ## anova
-    m2.anova <- Anova(modelt2, test="F")
-    
-    ## Check ele - amb diff
-    summ2 <- summary(glht(modelt2, linfct = mcp(Trt = "Tukey")))
-    
-    ## average effect size
-    eff.size2 <- coef(modelt2)[[1]][1,2]
-    
-    ## confidence interval 
-    eff.conf2 <- confint(modelt2,"Trtele")
+    #int.m2 <- "interative_with_covariate"
+    #modelt2 <- lmer(Value~Trt*Datef+Cov2 + (1|Ring),data=tDF)
+    #
+    ### anova
+    #m2.anova <- Anova(modelt2, test="F")
+    #
+    ### Check ele - amb diff
+    #summ2 <- summary(glht(modelt2, linfct = mcp(Trt = "Tukey")))
+    #
+    ### average effect size
+    #eff.size2 <- coef(modelt2)[[1]][1,2]
+    #
+    ### confidence interval 
+    #eff.conf2 <- confint(modelt2,"Trtele")
     
  
     ### conditional output
@@ -84,12 +85,12 @@ make_leafp_conc_treatment_abs_effect_statistics <- function(inDF,
                     eff = eff.size1,
                     conf = eff.conf1)
     } else if (stat.model == "interaction_with_covariate") {
-        out <- list(int.state=int.m2,
-                    mod = modelt2, 
-                    anova = m2.anova,
-                    diff = summ2,
-                    eff = eff.size2,
-                    conf = eff.conf2)
+        #out <- list(int.state=int.m2,
+        #            mod = modelt2, 
+        #            anova = m2.anova,
+        #            diff = summ2,
+        #            eff = eff.size2,
+        #            conf = eff.conf2)
     }
 
     ### Predict the model with a standard LAI value
